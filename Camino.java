1a9e22b8311ce188555e3e27d41a2dfbcfa429f2
NoeOsorio <36978686+NoeOsorio@users.noreply.github.com>
2018-03-08 20:05:13 -0600

    New files
    These are the files we are going to work with

---
+++ Camino.java
@@ -0,0 +1,6 @@
+public class Camino{
+  private int a;
+  private int b;
+  private int cost;
+
+}
---
+++ Dijkstra.java
@@ -0,0 +1,14 @@
+
+import java.util.ArrayList;
+
+public class Dijkstra{
+
+  //Arreglo de caminos
+  public ArrayList<Camino> Caminos = new ArrayList();
+  public int[][] grafo = new int[5][5];
+  public char[] nodos = new char[5];
+
+  public static void main(String[] args){
+
+  }
+}
