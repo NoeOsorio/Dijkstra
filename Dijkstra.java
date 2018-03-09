f40245e275aeef3b5365de0983a33502608f41ca
NoeOsorio <36978686+NoeOsorio@users.noreply.github.com>
2018-03-09 14:36:34 -0600

    Update Dijkstra
    Hey guys I kind of finished the original algorith what if we finish the original and then we just apply the modifications required to find the paths.

--- Dijkstra.java
+++ Dijkstra.java
@@ -1,12 +1,67 @@
 
+
 import java.util.ArrayList;
 
 public class Dijkstra{
 
+  public static final int N = 5;
   //Arreglo de caminos
-  public ArrayList<Camino> Caminos = new ArrayList();
-  public int[][] grafo = new int[5][5];
-  public char[] nodos = new char[5];
+  public ArrayList<Camino> Caminos = new ArrayList<Camino>();
+  //Conjunto S = {}
+  public ArrayList<String> S = new ArrayList<String>();
+  //Matriz de transicion del grafo
+  public int[][] grafo = new int[N][N];
+  //Arreglo de nodos = {a, b, c, d, e}
+  public String[] nodos = new String[N];
+  //Arreglo D = {}
+  public int[] D = new int[N];
+  //N-S
+  public ArrayList<String> NS = new ArrayList<String>();
+
+  //Constructor
+  public Dijkstra(int[][] grafo, String[] nodos){
+    this.grafo = grafo;
+    this.nodos = nodos;
+  }
+
+  private void N_menos_S(){
+    NS.clear();
+    for(int i = 0; i < nodos.length;i++){
+      if(!this.S.contains(this.nodos[i]))
+        NS.add(this.nodos[i]);
+    }
+  }
+
+  public String vertMinimo(){
+      String vertice = "";
+      //necesitamos hacer el algoritmo para calcula el vertice minimo
+
+      return vertice;
+  }
+
+  //Algoritmo principal Dijkstra Mejorado "Wink wink"
+  public void Djk(){
+    String w;
+    //S <- {a} //Nodo inicial
+    this.S.add(this.nodos[1]);
+    //para i <- 2 hasta (n - 1) hacer
+    for (int i = 2; i < N;i++)
+      //D[i] <- T[1][i]
+      D[i] = this.grafo[1][i];
+    //para 1 <- 1 hasta n-1 hacer
+    for (int i = 1;i < N-1; i++){
+      //Esto hace la operacion N-S
+      this.N_menos_S();
+      //Elige un vertice w tal que D[w] sea un minimo
+      w = this.vertMinimo();
+      //agrega w a S
+      this.S.add(w);
+      //para cada vertice y en N-S hacer
+      this.N_menos_S();
+
+    }
+  }
+
 
   public static void main(String[] args){
 
